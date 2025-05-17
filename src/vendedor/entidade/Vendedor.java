84540e6cc39f0a20a3bb50d6fd1affd78ea19738
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-05-17 17:40:14 -0300

    V.02

---
+++ src/vendedor/entidade/Vendedor.java
@@ -0,0 +1,111 @@
+package src.vendedor.entidade;
+
+public class Vendedor {
+
+    private String nome;
+    private static Double salarioInicial = 1.500;
+    private Integer tempoDeServico;
+    private char categoria;
+    private Double totalVendas;
+
+    public Vendedor(String nome, Integer tempoDeServico, Double totalVendas) {
+        this.nome = nome;
+        this.tempoDeServico = tempoDeServico;
+        this.totalVendas = totalVendas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static Double getSalarioInicial() {
+        return salarioInicial;
+    }
+
+    public static void setSalarioInicial(Double salarioInicial) {
+        Vendedor.salarioInicial = salarioInicial;
+    }
+
+    public Integer getTempoDeServico() {
+        return tempoDeServico;
+    }
+
+    public void setTempoDeServico(Integer tempoDeServico) {
+        this.tempoDeServico = tempoDeServico;
+    }
+
+    public char getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(char categoria) {
+        if (this.tempoDeServico > 60) {
+            this.categoria = 'A';
+        }
+        else if (this.tempoDeServico < 30) {
+            this.categoria = 'C';
+        }
+        else {
+            this.categoria = 'B';
+        }
+    }
+
+    public Double getTotalVendas() {
+        return totalVendas;
+    }
+
+    public void setTotalVendas(Double totalVendas) {
+        this.totalVendas = totalVendas;
+    }
+
+    @Override
+    public String toString() {
+        return nome +
+                "\n" +
+                tempoDeServico +
+                " horas \n" +
+                "Categoria: " +
+                categoria;
+    }
+
+//    comissao que calcula e retorna a comissão do vendedor de acordo com o valor de suas vendas: 20% do total de sua venda,
+//    se ela ultrapassou R$ 20.000,00, 15% se ela esteve entre R$ 10.000,00 e R$ 20.000,00 inclusive e 5% se suas vendas não
+//    chegaram a R$10.000,00.
+
+    public double comissao() {
+        if (this.totalVendas > 20000) {
+            return this.totalVendas * 0.2;
+        }
+        else if (this.totalVendas < 10000) {
+            return this.totalVendas * 0.05;
+        }
+        else {
+            return this.totalVendas * 0.15;
+        }
+    }
+
+//    gratificacao que calcula e retorna a gratificação do vendedor de acordo com sua categoria:  15% do salário base para
+//    Categoria ‘A’ e 7,5% para Categoria ‘B’.
+
+    public double gratificao() {
+        if (this.categoria == 'A') {
+            return this.salarioInicial * 0.15;
+        }
+        else if (this.categoria == 'B') {
+            return this.salarioInicial * 0.075;
+        }
+        else {
+            return 0;
+        }
+    }
+
+//    salario que calcula e retorna o salário do vendedor que é o seu salário base mais sua comissão mais sua gratificação.
+
+    public double salario() {
+        return this.salarioInicial + comissao() + gratificao();
+    }
+}
---
+++ src/vendedor/programa/Programa.java
@@ -0,0 +1,100 @@
+package src.vendedor.programa;
+
+import org.w3c.dom.ls.LSOutput;
+import src.vendedor.entidade.Vendedor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Programa {
+
+    static List<Vendedor> vendedorList = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
+    static final int TAM = 5;
+    static Vendedor vendedor;
+
+    public static void main(String[] args) {
+
+        int opcao;
+
+        do {
+            System.out.println("""
+                    1. Cadastrar Vendedor
+                    2. Mostrar Vendedor com Maior Tempo de Serviço
+                    3. Mostrar Nome e Categoria de Todos os Vendedores
+                    4. Imprimir Total de Vendas
+                    5. Remover Vendedor
+                    6. Fechar Programa""");
+
+            System.out.print("Qual a opcao: ");
+            opcao = scan.nextInt();
+
+            switch (opcao) {
+                case 1 -> cadastrar(vendedorList);
+                case 2 -> maiorTempo(vendedorList);
+                case 3 -> nomeCategoria(vendedorList);
+                case 4 -> totalVendas(vendedorList);
+                case 5 -> removerVendedor(vendedorList);
+                case 6 -> System.out.println("finalizando programa...");
+            }
+        }
+        while (opcao != 6);
+    }
+
+    public static void cadastrar(List<Vendedor> lista) {
+
+        for (int i = 0; i < TAM; i++) {
+            System.out.print("\nNome :");
+            scan.nextLine();
+            String nome = scan.nextLine();
+            System.out.print("Tempo de Serviço em Horas: ");
+            int tempo = scan.nextInt();
+            System.out.print("Total de Vendas: R$");
+            double vendas = scan.nextDouble();
+
+            vendedor = new Vendedor(nome, tempo, vendas);
+            lista.add(vendedor);
+        }
+    }
+
+    public static void maiorTempo(List<Vendedor> lista) {
+
+        lista.sort(Comparator.comparingInt(Vendedor :: getTempoDeServico).reversed());
+
+        for (Vendedor ven : lista) {
+            System.out.println('\n' + ven.getNome() + " - " + ven.getTempoDeServico() + " horas");
+        }
+    }
+
+    public static void nomeCategoria(List<Vendedor> lista) {
+
+        for (Vendedor ven : lista) {
+            System.out.println('\n' + ven.getNome() + " - Categoria: " + ven.getCategoria());
+        }
+    }
+
+    public static String totalVendas(List<Vendedor> lista) {
+
+        double soma = 0;
+
+        for (Vendedor ven : lista) {
+            soma = ven.getTotalVendas();
+        }
+
+        return String.format("Total de Vendas R$ %.2f" + soma);
+    }
+
+    public static void removerVendedor(List<Vendedor> lista) {
+        System.out.print("Digite o nome do Vendedor que deseja remover: ");
+        scan.nextLine();
+        String nome = scan.nextLine();
+
+        for (Vendedor ven : lista) {
+            if (nome.equalsIgnoreCase(ven.getNome())) {
+                lista.remove(ven);
+            }
+        }
+    }
+}
